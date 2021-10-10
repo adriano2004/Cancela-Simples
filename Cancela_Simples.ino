71ae5b2830668b9a0bb8b1f9105258e3a8a56ddc
Adriano dos Santos Lisboa <robertopereira23447@gmail.com>
2021-10-10 16:03:17 -0300

    Cancela Simples

---
+++ Cancela_Simples.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+
+Servo servo1;
+int bot=7;
+int val;
+
+
+void setup(){
+  servo1.attach(4);
+    pinMode(bot, INPUT);
+}
+
+void loop(){
+    val = digitalRead(bot);
+    servo1.write(0);
+   /* if( val ==1){
+    servo1.write(90);
+      delay(5000);
+      servo1.write(0);
+    }*/
+}
